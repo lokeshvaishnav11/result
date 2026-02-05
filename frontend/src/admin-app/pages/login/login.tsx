3c05d7d595586013b9bbe34533dfe1558b0d7fb7
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2026-02-05 15:45:44 +0530

    first commit

--- frontend/src/admin-app/pages/login/login.tsx
+++ frontend/src/admin-app/pages/login/login.tsx
@@ -38,10 +38,10 @@ const Login = () => {
       })
 
       if (userState.user.role && ['admin', '1', '2', '3'].includes(userState.user.role)) {
-        // return navigate.go('/list-clients')
+        return navigate.go('')
       }
 
-      // return navigate.go('/list-clients')
+      return navigate.go('')
     }
   }, [userState])
 
